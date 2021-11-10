8b0bda2c7fead2fdcf34de152b7f384e6208cd2f
flakee <flakeexe@gmail.com>
2021-11-10 19:22:00 +0300

    minor pep8 change

--- heatmap/management/commands/scraper.py
+++ heatmap/management/commands/scraper.py
@@ -107,7 +107,8 @@ def create_json_file(city: str, data: list) -> None:
 
 
 def run_scraper(self, city):
-    city=city or 'spb'
+    city = city or 'spb'
     pages = get_num_of_pages(city)
-    result = get_data_with_mp(city, 10)
+    pages = 10
+    result = get_data_with_mp(city, pages)
     create_json_file(city, result)
